61de2875131599f436555f87627a38d3fb9e3109
Abdul Haseeb Hamid <111380595+haseebx16@users.noreply.github.com>
2025-02-11 10:59:34 +0500

    page

--- app/Components/Login.jsx
+++ app/Components/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { font } from './font/font'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={` ${font.className} h-screen text-white flex justify-center flex-col items-center w-full bg-gradient-to-br from-sky-900 to-green-950`}>
+        <h1 className='text-5xl '>Login</h1>
+        <div className='bg-white'>
+
+        </div>
+    </div>
   )
 }
 
---
+++ app/Components/font/font.js
@@ -0,0 +1,6 @@
+import { Comfortaa } from "next/font/google";
+
+export const font = Comfortaa({
+    subsets: ['latin'],
+    weight: '400'
+})
